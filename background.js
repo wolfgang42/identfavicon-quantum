4a8863e108ac72af943794c01e19d51813d34117
Wolfgang Faust <wolfgang42@git>
2017-11-18 18:47:06 -0500

    Bugfix: workaround for uncached missing favicon.

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
+function inject(tab) {
+ 	browser.tabs.executeScript(tab.id, {
+		file: 'inject.js',
+	})
+}
+
 function setFavicon(tab) {
 	if (typeof tab.favIconUrl === 'undefined') {
-		browser.tabs.executeScript(tab.id, {
-			file: 'inject.js',
+		inject(tab)
+	} else if (/^https?:\/\/[^/]+\/favicon\.ico$/.test(tab.favIconUrl)) {
+		// Workaround when loading never-before-seen domains:
+		// If there's no cached favicon for a domain,
+		// Firefox blindly sets the tab's favIconUrl to /favicon.ico without knowing if it exists.
+		// We therefore fetch it ourselves (requesting the cached version if possible)
+		// and if the request does not succeed, then set the favicon to an identicon.
+		fetch(tab.favIconUrl, {cache: 'force-cache'}).then(r => {
+			if (!r.ok) inject(tab)
 		})
 	}
 }
