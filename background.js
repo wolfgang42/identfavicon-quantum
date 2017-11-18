87d74dfd98aa6eb003828b975e1c512a83c19170
Wolfgang Faust <wolfgang42@git>
2017-11-18 17:01:09 -0500

    Also set favicons when extension starts.

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (changeInfo.status && changeInfo.status === 'complete' && typeof tab.favIconUrl === 'undefined') {
-		browser.tabs.executeScript(tabId, {
+function setFavicon(tab) {
+	if (typeof tab.favIconUrl === 'undefined') {
+		browser.tabs.executeScript(tab.id, {
 			file: 'inject.js',
 		})
 	}
+}
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.status && changeInfo.status === 'complete') {
+		setFavicon(tab)
+	}
 })
+browser.tabs.query({status: 'complete'}).then(tabs => tabs.forEach(setFavicon))
