ad275b9bc23670e34dddd2e7281fda567287da7d
Wolfgang Faust <wolfgang42@git>
2018-11-05 21:55:08 -0500

    Add npm files and move building into scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p output
-cp -r src/manifest.json src/background.js icon/ output/
-cat src/blockies/blockies.js src/inject-core.js > output/inject.js
-
-web-ext lint -w -s output
