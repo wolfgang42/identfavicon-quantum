1ccda6e865f633891a216d19c827eceab93ebb83
Wolfgang Faust <wolfgang42@git>
2018-11-05 21:31:22 -0500

    Move code to src/

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p output
-cp -r manifest.json background.js icon/ output/
-cat blockies/blockies.js inject-core.js > output/inject.js
+cp -r src/manifest.json src/background.js icon/ output/
+cat src/blockies/blockies.js src/inject-core.js > output/inject.js
 
 web-ext lint -w -s output
