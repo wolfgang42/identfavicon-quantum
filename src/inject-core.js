b668d9134aebc74418162a09c1aa4c0e139c3d0a
Wolfgang Faust <wolfgang42@git>
2018-11-05 21:36:41 -0500

    Workaround for intermittent override of sites that already have a favicon.
    Closes #2.

--- src/inject-core.js
+++ src/inject-core.js
@@ -1,13 +1,17 @@
-const theIcon = blockies.create({
-	seed: document.location.host,
-})
+// Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1417721 :
+// check if the document already declares a <link rel="icon">
+// and don't inject ours if it does.
+if (!document.querySelector('link[rel~="icon"]')) {
+	const theIcon = blockies.create({
+		seed: document.location.host,
+	})
 
-const link = document.createElement('link')
-link.type='image/x-icon'
-link.rel='shortcut icon'
-link.href=theIcon.toDataURL('png')
-link.setAttribute('data-explanation', 'Injected by IdentFavIcon Quantum extension')
-document.head.appendChild(link)
+	const link = document.createElement('link')
+	link.type='image/x-icon'
+	link.rel='shortcut icon'
+	link.href=theIcon.toDataURL('png')
+	link.setAttribute('data-explanation', 'Injected by IdentFavIcon Quantum extension')
+	document.head.appendChild(link)
+}
 
 undefined // https://stackoverflow.com/a/44774834
-
